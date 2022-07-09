11a345fc598d8f560817579b515757ba18ef2197
minarefaat1002 <108765836+minarefaat1002@users.noreply.github.com>
2022-07-09 07:50:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        - The internet is an open and public system we all send and receive information over shared wired and connections .We also
-        exchange a lot of private data like credit card numbers, bank information, passwords and emails .
-        - Data of any kind can be kept secret through a process known as encryption .Which is defined as descrambling or changing
-         of the message to hide the original text.
-        - Decryption is the process of unscrambling that message to make it readable .
-        - One of the first well-known methods of encryption was caesar's cipher,named after julius caesar, A roman general who
-        encrypted his military commands to make sure that if a message was intercepted by enemies, they wouldn't be able to read it.
-        - Each encryption algorithms has a key allows the sender to encrypt the message and the receiver to decrypt the message.
-        - There are two types of encryption symmetric and asymmetric.
-        - When the sender and receiver share the same key to scramble and unscramble a message . it's called symmetric encryption
-        like caesar's cipher.
-        - When there's public key to encrypt data but the secred can only be decrypted by computer with access to the private key.
-        - Our project is about one-time pad encryption (OTP) which is symmetric encryption .
-        -OTP is an encryption technique that cannot be cracked, but it requires the following
-            1- Random key that is as long as the message.
-            2- The key is to be used to encrypt and decrypt a single message and then it is discarded.
-            3- The must be securely shared between the sending and receiving parties
-         */
         EncryptionApp encryptionApp = new EncryptionApp();
     }
 }
